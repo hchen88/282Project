0faeffe62dc49d2df45a10327e324730ee50b095
areema578 <30280730+areema578@users.noreply.github.com>
2018-11-20 17:26:35 -0800

    Add files via upload

--- BlackJackSlotMachine.cpp
+++ BlackJackSlotMachine.cpp
@@ -10,27 +10,28 @@ using namespace std;
 int main(int argc, char** argv) {
 	
 	double pot;
-	double totalBet = 0;
-	double amountWon = 0;
+	double totalBet = 0; //overall amount bet
+	double amountWon = 0; //overall earnings
+	bool play = true;
+	bool split = false;
 	cout<<fixed<<setprecision(2);
 	cout<<"We are going to play Blackjack."<<endl;
 	cout<<"Enter your account number: ";
 	string accountNumber;
 	cin>>accountNumber;
 	Account account(accountNumber);
+	//This part might need to be inside the do-while loop (unsure)
 	cout<<"Your current balance is $"<<account.getMoney()<<endl;
 	cout<<"Enter the amount of money you would like to bet: ";
 	double money;
 	cin>>money;
 	account.bet(money);
-	pot = money;
-	bool play = true;
-	bool split = false;
+	pot = money; //amount for the round
 	do{
 		Player dealer();
 		Player user();
 		
-		play = false;
+		play = false; //when the user doesn't want to play again
 	}
 	while(play);
 	cout<<"Total amount of betting money: $"<<totalBet<<endl;
